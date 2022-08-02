56232f81dfd30f80f14478ff49265318645555a5
danielliucs <daniell.liu@mail.utoronto.ca>
2022-08-02 11:23:55 -0400

    first activity implemeneted

--- numGuessGame_StudentVer/checkingNum.py
+++ numGuessGame_StudentVer/checkingNum.py
@@ -1,2 +1,9 @@
 #Write your functions on checking input here
 
+def check_num(my_guess, actual_num):
+    if my_guess < actual_num:
+        return "Your guess is lower than the actual number"
+    elif my_guess > actual_num:
+        return "Your guess is higher than the actual number"
+    else:
+        return "You guessed the number!"
--- numGuessGame_StudentVer/main.py
+++ numGuessGame_StudentVer/main.py
@@ -1,10 +1,24 @@
 import random # importing random module
-import checkingNum # importing file called checkingNum.py
+#import checkingNum # importing file called checkingNum.py
+from checkingNum import check_num
 
 def main(): #Fill in the blank lines inside the function, be sure to delete pass
     runGame = True # keeps game running
-    
-    while runGame == True: # only 5 tries allowed
-        pass
+    tries = 0
+    number_generated = random.randint(1, 100)
+    while runGame == True: # only 7 tries allowed
+        number_inputed = int(input("Enter your number: "))
+        if number_generated == number_inputed:
+            print("You guessed the number!")
+            break
+        
+        output = check_num(number_inputed, number_generated)
+        print(output)
+        tries += 1
+        if tries == 7:
+            print("You did not get the number :(")
+            break
+        
+
 
 main() # call main function (Note: not necessary but done anyways - see reason here: https://stackoverflow.com/questions/4041238/why-use-def-main )
