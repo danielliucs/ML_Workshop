c87187f16b924ce233c7a5cd81b300c2418f108e
danielliucs <daniell.liu@mail.utoronto.ca>
2022-08-02 14:13:10 -0400

    update gitignore

--- supermarketProgram_Student_Ver/main.py
+++ supermarketProgram_Student_Ver/main.py
@@ -4,7 +4,6 @@ def main():
     runProgram = True # boolean
     print("Welcome.") # welcom messages displayed in terminal
 
-
     while runProgram == True: # while loop keeps program running
         option = int(input("Select:\n1 - View all items\n2 - Remove an item\n3 - Add an item\n4 - Change an item\n5 - Create CSV file\n6 - Exit program\n")) # gets user input
         
